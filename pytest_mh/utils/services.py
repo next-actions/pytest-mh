539d1f397369eddd88e0fb5c23bb705a41047d08
Pavel Březina <pbrezina@redhat.com>
2023-11-15 17:50:41 +0100

    systemd: add log messages to systemd

--- pytest_mh/utils/services.py
+++ pytest_mh/utils/services.py
@@ -19,9 +19,17 @@ class SystemdServices(MultihostUtility):
         # Restart all services that were touched
         self.reload_daemon()
         for service, state in self.initial_states.items():
-            self.host.ssh.run(f'systemctl stop "{service}" || systemctl status "{service}"', raise_on_error=False)
+            self.host.ssh.run(
+                f'systemctl stop "{service}" || systemctl status "{service}"',
+                raise_on_error=False,
+                log_level=SSHLog.Error,
+            )
             if state:
-                self.host.ssh.run(f'systemctl start "{service}" || systemctl status "{service}"', raise_on_error=False)
+                self.host.ssh.run(
+                    f'systemctl start "{service}" || systemctl status "{service}"',
+                    raise_on_error=False,
+                    log_level=SSHLog.Error,
+                )
 
     def async_start(self, service: str) -> SSHProcess:
         """
@@ -36,7 +44,10 @@ class SystemdServices(MultihostUtility):
         :rtype: SSHProcess
         """
         self.__set_initial_state(service)
-        return self.host.ssh.async_run(f'systemctl start "{service}" || systemctl status "{service}"')
+        self.logger.info(f'systemd: starting "{service}" asynchronously')
+        return self.host.ssh.async_run(
+            f'systemctl start "{service}" || systemctl status "{service}"', log_level=SSHLog.Error
+        )
 
     def start(self, service: str, raise_on_error: bool = True) -> SSHProcessResult:
         """
@@ -53,8 +64,11 @@ class SystemdServices(MultihostUtility):
         :rtype: SSHProcessResult
         """
         self.__set_initial_state(service)
+        self.logger.info(f'systemd: starting "{service}"')
         return self.host.ssh.run(
-            f'systemctl start "{service}" || systemctl status "{service}"', raise_on_error=raise_on_error
+            f'systemctl start "{service}" || systemctl status "{service}"',
+            raise_on_error=raise_on_error,
+            log_level=SSHLog.Error,
         )
 
     def async_stop(self, service: str) -> SSHProcess:
@@ -70,7 +84,10 @@ class SystemdServices(MultihostUtility):
         :rtype: SSHProcess
         """
         self.__set_initial_state(service)
-        return self.host.ssh.async_run(f'systemctl stop "{service}" || systemctl status "{service}"')
+        self.logger.info(f'systemd: stopping "{service}" asynchronously')
+        return self.host.ssh.async_run(
+            f'systemctl stop "{service}" || systemctl status "{service}"', log_level=SSHLog.Error
+        )
 
     def stop(self, service: str, raise_on_error: bool = True) -> SSHProcessResult:
         """
@@ -87,8 +104,11 @@ class SystemdServices(MultihostUtility):
         :rtype: SSHProcessResult
         """
         self.__set_initial_state(service)
+        self.logger.info(f'systemd: stopping "{service}"')
         return self.host.ssh.run(
-            f'systemctl stop "{service}" || systemctl status "{service}"', raise_on_error=raise_on_error
+            f'systemctl stop "{service}" || systemctl status "{service}"',
+            raise_on_error=raise_on_error,
+            log_level=SSHLog.Error,
         )
 
     def async_restart(self, service: str) -> SSHProcess:
@@ -104,7 +124,10 @@ class SystemdServices(MultihostUtility):
         :rtype: SSHProcess
         """
         self.__set_initial_state(service)
-        return self.host.ssh.async_run(f'systemctl restart "{service}" || systemctl status "{service}"')
+        self.logger.info(f'systemd: restarting "{service}" asynchronously')
+        return self.host.ssh.async_run(
+            f'systemctl restart "{service}" || systemctl status "{service}"', log_level=SSHLog.Error
+        )
 
     def restart(self, service: str, raise_on_error: bool = True) -> SSHProcessResult:
         """
@@ -121,8 +144,11 @@ class SystemdServices(MultihostUtility):
         :rtype: SSHProcessResult
         """
         self.__set_initial_state(service)
+        self.logger.info(f'systemd: restarting "{service}"')
         return self.host.ssh.run(
-            f'systemctl restart "{service}" || systemctl status "{service}"', raise_on_error=raise_on_error
+            f'systemctl restart "{service}" || systemctl status "{service}"',
+            raise_on_error=raise_on_error,
+            log_level=SSHLog.Error,
         )
 
     def async_reload(self, service: str) -> SSHProcess:
@@ -137,7 +163,10 @@ class SystemdServices(MultihostUtility):
         :return: Running SSH process.
         :rtype: SSHProcess
         """
-        return self.host.ssh.async_run(f'systemctl reload "{service}" || systemctl status "{service}"')
+        self.logger.info(f'systemd: reloading "{service}" asynchronously')
+        return self.host.ssh.async_run(
+            f'systemctl reload "{service}" || systemctl status "{service}"', log_level=SSHLog.Error
+        )
 
     def reload(self, service: str, raise_on_error: bool = True) -> SSHProcessResult:
         """
@@ -153,8 +182,11 @@ class SystemdServices(MultihostUtility):
         :return: SSH process result.
         :rtype: SSHProcessResult
         """
+        self.logger.info(f'systemd: reloading "{service}"')
         return self.host.ssh.run(
-            f'systemctl reload "{service}" || systemctl status "{service}"', raise_on_error=raise_on_error
+            f'systemctl reload "{service}" || systemctl status "{service}"',
+            raise_on_error=raise_on_error,
+            log_level=SSHLog.Error,
         )
 
     def async_status(self, service: str) -> SSHProcess:
@@ -188,7 +220,8 @@ class SystemdServices(MultihostUtility):
         :return: Running SSH process.
         :rtype: SSHProcess
         """
-        return self.host.ssh.async_run("systemctl daemon-reload")
+        self.logger.info("systemd: reloading systemd daemon")
+        return self.host.ssh.async_run("systemctl daemon-reload", log_level=SSHLog.Error)
 
     def reload_daemon(self, raise_on_error: bool = True) -> SSHProcessResult:
         """
